be602a3a68ebd7f8738828f5c3a648e9708832b3
John Foushee <johnfoushee@gmail.com>
2014-10-28 11:19:20 -0400

    Updating module config.

--- module/config/module.config.php
+++ module/config/module.config.php
@@ -2,13 +2,39 @@
 return array(
 	'service_manager' => array(
 		'invokables' => array(
-			'FzyZF2Common\Service\EntityToForm' => 'FzyZF2Common\Service\EntityToForm'
+			'FzyCommon\Service\EntityToForm' => 'FzyCommon\Service\EntityToForm',
+			'FzyCommon\Service\Flattener' => 'FzyCommon\Service\Flattener',
 		),
 		'factories' => array(
 
 		),
 		'abstract_factories' => array(
 
+		),
+	),
+	'controller_plugins' => array(
+		'invokables' => array(
+			'searchResult' => 'FzyCommon\Controller\Plugin\SearchResult',
+			'updateResult' => 'FzyCommon\Controller\Plugin\UpdateResult',
+			'e2f'          => 'FzyCommon\Controller\Plugin\EntityToForm',
+		)
+	),
+	'view_helpers' => array(
+		'invokables' => array(
+			'e2f'            => 'FzyCommon\View\Helper\EntityToForm',
+			'ngInit'         => 'FzyCommon\View\Helper\NgInit',
+		),
+		'factories' => array(
+			'flashMessages' => function($sm) {
+				$flashmessenger = $sm->getServiceLocator()
+				                     ->get('ControllerPluginManager')
+				                     ->get('flashmessenger');
+
+				$messages = new \FzyCommon\View\Helper\FlashMessages();
+				$messages->setFlashMessenger($flashmessenger);
+
+				return $messages;
+			},
 		),
 	),
 );
