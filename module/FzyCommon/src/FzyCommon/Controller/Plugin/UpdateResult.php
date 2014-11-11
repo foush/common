f3961908700b0358c5d5a4db38db1074b94e841c
John Foushee <johnfoushee@gmail.com>
2014-11-11 11:42:53 -0500

    Fixing namespace bug on update plugin.

--- module/FzyCommon/src/FzyCommon/Controller/Plugin/UpdateResult.php
+++ module/FzyCommon/src/FzyCommon/Controller/Plugin/UpdateResult.php
@@ -2,49 +2,49 @@
 
 namespace FzyCommon\Controller\Plugin;
 
-use FzyCommon\Service\Update;
+use FzyCommon\Service\Update\Base as Update;
 use Zend\Http\PhpEnvironment\Response;
 use Zend\View\Model\JsonModel;
 
 class UpdateResult extends Base
 {
-    public function __invoke(Update $updater)
-    {
-        $redirect = null;
-        if ($updater->getValid()) {
-            $this->flashMessenger()->addSuccessMessage($updater->getFormattedSuccessMessage());
-            $redirect = $this->url()->fromRoute($updater->getSuccessRedirectRouteName(), $updater->getSuccessRedirectRouteParams(), $updater->getSuccessRedirectRouteOptions());
-        } else {
-	        // set error
-	        /* @var $response \Zend\Http\PhpEnvironment\Response */
-	        $response = $this->getService('Response');
-	        $response->setStatusCode(Response::STATUS_CODE_400);
-        }
+	public function __invoke(Update $updater)
+	{
+		$redirect = null;
+		if ($updater->getValid()) {
+			$this->flashMessenger()->addSuccessMessage($updater->getFormattedSuccessMessage());
+			$redirect = $this->url()->fromRoute($updater->getSuccessRedirectRouteName(), $updater->getSuccessRedirectRouteParams(), $updater->getSuccessRedirectRouteOptions());
+		} else {
+			// set error
+			/* @var $response \Zend\Http\PhpEnvironment\Response */
+			$response = $this->getService('Response');
+			$response->setStatusCode(Response::STATUS_CODE_400);
+		}
 
-        return array(
-            'success' => $updater->getValid(),
-            'messages' => $updater->getErrorMessages(),
-            'models' => $updater->getEntitiesAsJson(),
-            'redirect' => $redirect,
-        );
-    }
+		return array(
+			'success' => $updater->getValid(),
+			'messages' => $updater->getErrorMessages(),
+			'models' => $updater->getEntitiesAsJson(),
+			'redirect' => $redirect,
+		);
+	}
 
-    /**
-     * @return \Zend\Mvc\Controller\Plugin\FlashMessenger
-     */
-    protected function flashMessenger()
-    {
-        return $this->getService('ControllerPluginManager')
-            ->get('flashmessenger');
-    }
+	/**
+	 * @return \Zend\Mvc\Controller\Plugin\FlashMessenger
+	 */
+	protected function flashMessenger()
+	{
+		return $this->getService('ControllerPluginManager')
+		            ->get('flashmessenger');
+	}
 
-    /**
-     * @return \Zend\Mvc\Controller\Plugin\Url
-     */
-    protected function url()
-    {
-        return $this->getService('ControllerPluginManager')
-            ->get('url');
-    }
+	/**
+	 * @return \Zend\Mvc\Controller\Plugin\Url
+	 */
+	protected function url()
+	{
+		return $this->getService('ControllerPluginManager')
+		            ->get('url');
+	}
 
 }
